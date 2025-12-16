3b2789c952abc9663adfa68f4b39f789b4c7ae35
oxanashva <oxanashav@gmail.com>
2025-12-16 02:39:12 +0200

    init: basic project structure

---
+++ config/prod.js
@@ -0,0 +1,19 @@
+export default {
+    dbURL: process.env.MONGO_URL,
+    dbName: process.env.DB_NAME
+}
+
+// export default {
+//     dbURL: process.env.MONGO_URL || 'mongodb+srv://{USERNAME}:{PASSWORD}@cluster0.6qm6pd1.mongodb.net/',
+//     dbName: process.env.DB_NAME || 'car_db'
+// }
+
+// export default {
+//     dbURL: process.env.MONGO_URL,
+//     dbName: process.env.DB_NAME '
+// }
+
+
+
+
+
